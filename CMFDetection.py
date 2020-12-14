bd6b24331d30ba268693f3f4fa8c191512f7e0f5
HokageHimanshu <himanshu17291@iiitd.ac.in>
2020-12-15 01:58:26 +0530

    analysis function added

--- CMFDetection.py
+++ CMFDetection.py
@@ -2,46 +2,49 @@ from sklearn.cluster import DBSCAN
 import numpy as np
 import cv2
 
-class ForgeryDetector(object):
+def run_sift_detection(original_image):
+    sift = cv2.xfeatures2d.SIFT_create()
+    # print('sift is')
+    # print(sift)
+    gray= cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY) 
+    key_points,descriptors = sift.detectAndCompute(gray, None)
+    # print('key_points are ')
+    # print(self.key_points)
+    # print(self.descriptors)
+    # print()
+    return key_points,descriptors
 
-	def __init__(self, original_image):
-		self.original_image = original_image
-		self.key_points = None
-		self.descriptors = None
+def show_sift_features(original_image,key_points,descriptors):
+    gray_original_image=cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY)
+    temp_image = original_image.copy()
+    sift_original_image=cv2.drawKeypoints(original_image,key_points,temp_image)
+    # sift_original_image=cv2.drawMatches(self.original_image,self.key_points,self.original_image.copy())
+    return sift_original_image
 
-	def run_sift_detection(self):
-	    sift = cv2.xfeatures2d.SIFT_create()
-	    # print('sift is')
-	    # print(sift)
-	    gray= cv2.cvtColor(self.original_image,cv2.COLOR_BGR2GRAY) 
-	    self.key_points,self.descriptors = sift.detectAndCompute(gray, None)
-	    # print('key_points are ')
-	    # print(self.key_points)
-	    # print(self.descriptors)
-	    # print()
-	    return self.key_points,self.descriptors
+def find_clusters(clusters,size,key_points):
+	cluster_list= [[] for i in range(size)]
+	for idx in range(len(key_points)):
+	    if clusters.labels_[idx]!=-1:
+	        cluster_list[clusters.labels_[idx]].append((int(key_points[idx].pt[0]),int(key_points[idx].pt[1])))
+	return cluster_list
 
-	def show_sift_features(self):
-	    gray_original_image=cv2.cvtColor(self.original_image,cv2.COLOR_BGR2GRAY)
-	    sift_original_image=cv2.drawKeypoints(self.original_image,self.key_points,self.original_image.copy())
-	    # sift_original_image=cv2.drawMatches(self.original_image,self.key_points,self.original_image.copy())
-	    return sift_original_image
-		
-	def find_forgery(self,eps=40,min_sample=2):
-		clusters=DBSCAN(eps=eps, min_samples=min_sample).fit(self.descriptors)
-		size=np.unique(clusters.labels_).shape[0]-1
-		manipulated_image=self.original_image.copy()
-		if (size==0) and (np.unique(clusters.labels_)[0]==-1):
-			print('No manipulated_image Found!!')
-			return None
-		if size==0:
-			size=1
-		cluster_list= [[] for i in range(size)]
-		for idx in range(len(self.key_points)):
-		    if clusters.labels_[idx]!=-1:
-		        cluster_list[clusters.labels_[idx]].append((int(self.key_points[idx].pt[0]),int(self.key_points[idx].pt[1])))
-		for points in cluster_list:
-		    if len(points)>1:
-		        for idx1 in range(1,len(points)):
-		            cv2.line(manipulated_image,points[0],points[idx1],(0,255,0),2)
+def draw_line(cluster_list,manipulated_image):
+	for points in cluster_list:
+	    if len(points)>1:
+	        for idx1 in range(1,len(points)):
+	            cv2.line(manipulated_image,points[0],points[idx1],(255,255,255),2)
+	return manipulated_image
+
+def find_forgery(original_image,key_points,descriptors,eps=40,min_sample=2):
+	clusters=DBSCAN(eps=eps, min_samples=min_sample).fit(descriptors)
+	size=np.unique(clusters.labels_).shape[0]-1
+	manipulated_image=original_image.copy()
+	if (size==0) and (np.unique(clusters.labels_)[0]==-1):
+		print('No manipulated_image Found!!')
+		# return None
 		return manipulated_image
+	if size==0:
+		size=1
+	cluster_list=find_clusters(clusters,size,key_points)
+	manipulated_image = draw_line(cluster_list,manipulated_image)
+	return manipulated_image
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import sys
 import cv2
-from CMFDetection import ForgeryDetector
+from CMFDetection import *
 import re
 from datetime import datetime
+import math
+import matplotlib.pyplot as plt
+import numpy as np
 
+def perform_cmfd(fname,original_image,eps,min_samples,wait_time,saveit=False,showit=False):
+	key_points,descriptors = run_sift_detection(original_image)
 
-print('Welcome to the Copy Move original_image manipulated_image Detection Application!\n')
-choice = 'y'
-while choice.lower() == 'y':
+	manipulated_image=find_forgery(original_image,key_points,descriptors,eps,min_samples)
+
+	if manipulated_image is None:
+		sys.exit(0) 
+	if saveit:
+		save_images(original_image,manipulated_image,fname)
+
+	# wait_time=15000
+	if showit:
+		cv2.imshow('Original original_image',original_image)
+		cv2.imshow('manipulated_image',manipulated_image)
+		cv2.imshow('SIFT features',show_sift_features(original_image,key_points,descriptors))
+		keyCode = cv2.waitKey(wait_time)
+		cv2.destroyAllWindows()
+	return (original_image,manipulated_image)
+
+def format_name(fname):
+	return fname.split('/')[1].split('.')[0]
+
+def save_images(original_image,manipulated_image,fname):
+	fname = format_name(fname)
+	cv2.imwrite('Original'+fname+'.jpg', original_image) 
+	cv2.imwrite('Forged'+fname+'.jpg', manipulated_image)
+
+def read_image():
 	fname  = input('\nEnter the path/name of the original_image = ')
 	fname = fname.rstrip()
 	assert(fname!="")
@@ -19,57 +46,84 @@ while choice.lower() == 'y':
 	if original_image is None:
 		print('File path/name entered by you is ',fname,'\nKindly enter a valid file path')
 		sys.exit(0)
+	return (fname,original_image)
 
-	# eps is in range of (0,500)
-	# min_sample is in range of (0,50)
+def convert_image_dimensions(image):
+	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# n,m,o = image.shape
+	# return image[:,:,1].reshape(n,m)
 
-	eps =60
-	min_samples=2
+def test_cmfd(original_image,eps_list,min_samples_list,wait_time,fname):
+	# eps_list = [60,100,400]
+	# min_samples_list=[2,5,20]
+	# wait_time=15000
+	titles = ['Original Image'] 
+	images = [convert_image_dimensions(original_image)]
+	for eps in eps_list:
+		for min_samples in min_samples_list:
+			# eps = int(input('Enter value of eps (0,500) or 60 for default value = '))
+			# min_samples = int(input('Enter value of minimum no of samples (0,50) or 2 for default value = '))
+			# print('\nStarting the process of detecting manipulated_image with parameter values as the following')
+			print('\nRunning sift algo for')
+			print('Eps = ',eps)
+			print('minimum no. of samples = ',min_samples)
+			original_image,manipulated_image = perform_cmfd(fname,original_image,eps,min_samples,wait_time,saveit=False,showit=False)
+			titles.append('FI_eps='+str(eps)+'_minsamples='+str(min_samples))
+			images.append(convert_image_dimensions(manipulated_image))
+	plot_graph(titles,images,fname)
+
+def plot_graph(titles,images,fname):
+	n = len(images)
+	nrows = math.ceil(math.sqrt(n))
+	ncols = math.ceil(n/nrows)
+	x = range(10)
+	y = range(10)
+	fig, ax = plt.subplots(nrows=nrows, ncols=ncols)
+	index =0
+	for r in ax:
+	    for c in r:
+	    	c.axis('off')
+	    	if index<n:
+		        c.imshow(images[index])
+		        c.set_title(titles[index],size=8)
+		        # c.text(0.5,-0.1,titles[index], size=10, ha="center")
+		        # c.axis('off')
+		        index+=1
+		    # c.axis('off')
+	fname = format_name(fname)
+	plt.savefig('analysis_'+fname+'.png')
+	plt.show()
 
 
-	eps = int(input('Enter value of eps (0,500) or 60 for default value = '))
-	min_samples = int(input('Enter value of minimum no of samples (0,50) or 2 for default value = '))
+print('Welcome to the Copy Move original_image manipulated_image Detection Application!\n')
+choice = 'y'
+while choice.lower() == 'y':
+	print('MENU - ')
+	print('1. Apply CMFD for one image')
+	print('2. Test CMFD for eps and min_samples')
+	menu_option = int(input('Enter your option (1/2): '))
+	if menu_option==1:
+		fname,original_image = read_image()
 
+		eps =60
+		min_samples=2
+		wait_time=15000
 
-	print('\nStarting the process of detecting manipulated_image with parameter values as the following')
-	print('Eps = ',eps)
-	print('minimum no. of samples = ',min_samples)
+		eps = int(input('Enter value of eps (0,500) or 60 for default value = '))
+		min_samples = int(input('Enter value of minimum no of samples (0,50) or 2 for default value = '))
 
-	forgery_finder=ForgeryDetector(original_image)
+		print('\nStarting the process of detecting manipulated_image with parameter values as the following')
+		print('Eps = ',eps)
+		print('minimum no. of samples = ',min_samples)
 
-	key_points,descriptors = forgery_finder.run_sift_detection()
+		original_image,manipulated_image = perform_cmfd(fname,original_image,eps,min_samples,wait_time,saveit=True,showit=True)
 
-	manipulated_image=forgery_finder.find_forgery(eps,min_samples)
-
-	if manipulated_image is None:
-		sys.exit(0)
-
-	cv2.imwrite('Original.jpg', original_image) 
-	cv2.imwrite('Forged.jpg', manipulated_image) 
-
-	cv2.imshow('Original original_image',original_image)
-	cv2.imshow('manipulated_image',manipulated_image)
-	cv2.imshow('SIFT features',forgery_finder.show_sift_features())
-
-	wait_time=10000
-	keyCode = cv2.waitKey(wait_time)
-	cv2.destroyAllWindows()
-	choice = input('Enter (y/Y) to continue = ')
-	# if choice.lower()=='y':
-	# 	continue
-	# else:
-		
-	# while(cv2.getWindowProperty('manipulated_image', 0) >= 0) or (cv2.getWindowProperty('Original original_image', 0) >= 0) :
-	# 	keyCode = cv2.waitKey(wait_time)
-	# 	if (keyCode) == ord('q') or keyCode==ord('Q'):
-	# 		cv2.destroyAllWindows()
-	# 		break
-	# 	elif keyCode == ord('s') or keyCode ==ord('S'):
-	# 		name=re.findall(r'(.+?)(\.[^.]*$|$)',fname)
-	# 		date=datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
-	# 		new_fname=name[0][0]+'_'+str(eps)+'_'+str(min_samples)
-	# 		new_fname=new_fname+'_'+date+name[0][1]
-	# 		PrintBoundary()
-			
-	# 		vaue=cv2.imwrite(new_fname,manipulated_image)
-	# 		print('original_image Saved as....',new_fname)
+	elif menu_option==2:
+		eps_list = [60,100,200]
+		min_samples_list=[2,5,10,45]
+		wait_time=15000
+		fname, original_image = read_image()
+		test_cmfd(original_image,eps_list,min_samples_list,wait_time,fname)
+	else:
+		print('Invalid Option entered')
+	choice = input('\nEnter (y/Y) to continue = ')
